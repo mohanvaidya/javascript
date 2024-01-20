92c654f3958273cb37327efd23722dea3e567bfb
Mohan Vaidya <131249018+mohanvaidya@users.noreply.github.com>
2024-01-20 23:08:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -230,3 +230,41 @@ for (const flight of flights.split('+')) {
   console.log(output);
 }
 
+
+--------------------New challenge on function --------------------
+
+const poll = {
+  question: ['What is your favourite programming language?'],
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3:C++'],
+  // This generates [0, 0, 0, 0]. More in the next section!
+  answers: new Array(4).fill(0),
+  registerNewUser() {
+    const answer = Number(
+      prompt(
+        `${this.question}\n ${this.options.toString('\n')}(write option number)`
+      )
+    );
+    console.log(answer);
+    //Register answer
+    typeof answer === 'number' &&
+      answer < this.options.length &&
+      this.answers[answer]++;
+    console.log(this.answers);
+    this.displayResult();
+    this.displayResult('string');
+  },
+  displayResult(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      console.log(`Pollresults are ${this.answers.join(', ')}`);
+    }
+  },
+};
+poll.registerNewUser();
+
+document
+  .querySelector('.btn')
+  .addEventListener('click', poll.registerNewUser.bind(poll));
+
+poll.displayResult.call({ answers: [5, 2, 3] }, 'string');
